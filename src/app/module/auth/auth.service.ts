513bb570abf8b75458934c5dbf19bead59c26899
mdrasel97 <mollarasel972@gmail.com>
2026-02-08 10:12:44 +0600

    patient migrate

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,6 +1,7 @@
 
 import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
+import { prisma } from "../../lib/prisma";
 
 
 interface ILoginUserPayload {
@@ -33,14 +34,23 @@ if (!data.user) {
     throw new Error("Failed to register user")
 }
 
-// const patient = await prisma.$transaction(async (tx) => {
-// await tx.
-// })
+const patient = await prisma.$transaction(async (tx) => {
 
-// return {
-//     user: data.user
-// }
-return data
+    const patientTx = await tx.patient.create({
+        data: {
+            userId: data.user.id,
+            name: payload.name,
+            email: payload.email,
+        }    
+    })
+    return patientTx
+})
+
+return {
+    ...data,
+    patient
+}
+
 }
 
 
