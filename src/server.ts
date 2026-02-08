8374455d7afa4b2f82bf8b5a04c06c084fb01fb0
mdrasel97 <mollarasel972@gmail.com>
2026-02-08 10:01:58 +0600

    patient model added

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,12 @@ import { envVers } from "./config/env";
 
 dotenv.config();
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`🔥 Healthcare server running on port ${process.env.PORT}`);
-// });
-
 
 const bootstrap = async () => {
   try {
     app.listen(envVers.PORT, () => {
       console.log(`🔥 Healthcare server running on port ${envVers.PORT}`);
-    });
+    }); 
   }catch (error) {
     console.error("Error starting the server:", error);
     process.exit(1);
